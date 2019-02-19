c127f8ed934a98f514bca43bb7904d81ff96ca12
Dimitrios Intziadis <2414831i@student.gla.ac.uk>
2019-02-19 18:21:02 +0000

    Cookfilapp added. Bootstrap added.

--- cookflix/settings.py
+++ cookflix/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Static folders
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Application definition
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookflixapp'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'cookflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
