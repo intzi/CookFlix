0fc147c4c57979e5ea6504333c9321c4a48f487f
Dimitrios Intziadis <intzi@outlook.com>
2019-03-08 22:35:14 +0000

    Upload Recipe - Fixed file accept types and choose file disign

--- static/customjs/customjs.js
+++ static/customjs/customjs.js
@@ -1,10 +1,16 @@
-$("[type=file]").on("change", function(){
-  // Name of file and placeholder
-  var file = this.files[0].name;
-  var dflt = $(this).attr("placeholder");
-  if($(this).val()!=""){
-    $(this).next().text(file);
-  } else {
-    $(this).next().text(dflt);
-  }
+$(document).ready(function(){
+  $("[type=file]").on("change", function(){
+    // Name of file and placeholder
+    var file = this.files[0].name;
+    var dflt = $(this).attr("name");
+    var prefix = "Selected thumbnail: ";
+    if(dflt == "video_file"){
+          prefix = "Selected video: ";
+    }
+    if($(this).val()!=""){
+      $(this).next().text(prefix+file);
+    } else {
+      $(this).next().text(dflt);
+    }
+  });
 });
