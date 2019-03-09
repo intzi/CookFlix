cb228ada7a57e48d65eedfb1e05ac8d6dafbd63e
Dimitrios Intziadis <intzi@outlook.com>
2019-03-09 00:09:00 +0000

    Recipe Video Changes

--- cookflix/urls.py
+++ cookflix/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from cookflixapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
@@ -16,3 +18,4 @@ urlpatterns = [
     url(r'^search/$', views.search, name='search'),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
